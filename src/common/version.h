b54e6ef89c6705ca5a541940ba282679c1d1c229
Doug Smith <Doug.Smith@lairdtech.com>
2017-05-25 17:26:02 -0400

    BZ11314: initial sterling supplicant
    Add Laird version number.
    Add laird directory with:
    - README
    - build configuration
    - Makefile
    
    Bug: 11314

--- src/common/version.h
+++ src/common/version.h
@@ -12,3 +12,5 @@
 #define VERSION_STR "2.6" VERSION_STR_POSTFIX GIT_VERSION_STR_POSTFIX
 
 #endif /* VERSION_H */
+
+#include "version_laird.h"
---
+++ src/common/version_laird.h
@@ -0,0 +1,23 @@
+#ifndef VERSION_LAIRD_H
+#define VERSION_LAIRD_H
+/* Laird version number */
+#define SUPPVER1	40
+#define SUPPVER2	4
+#define SUPPVER3	0
+#define	SUPPVER4	0
+
+#define _MACRO2STR(x) #x
+#define MACRO2STR(x) _MACRO2STR(x)
+#define LAIRD_VERSION_STR_POSTFIX \
+ "-Laird-" \
+ MACRO2STR(SUPPVER1) "."	 \
+ MACRO2STR(SUPPVER2) "." \
+ MACRO2STR(SUPPVER3) "." \
+ MACRO2STR(SUPPVER4)
+
+/* Modify version string from <version.h> */
+#undef VERSION_STR
+#define VERSION_STR "2.6" VERSION_STR_POSTFIX GIT_VERSION_STR_POSTFIX \
+	LAIRD_VERSION_STR_POSTFIX
+
+#endif /* VERSION_LAIRD_H */
